e036b42347954049aef52c2df2faf93006663153
James Winkle <jjwinkle@sabine.rcdc.uh.edu>
2020-08-18 17:36:00 -0500

    added Sabine folder for shell scripts

---
+++ sabine/jbuild.sh
@@ -0,0 +1,19 @@
+module purge
+# module load cmake fenics PETSc MPFR/4.0.2-GCCcore-6.4.0
+
+module load FENICS 
+
+echo "module list"
+module list
+
+
+echo $PETSC_DIR
+
+touch ./eQ/CMakeLists.txt  #re-scans git branch and hash
+touch ./eQ/src/inputOutput.cpp
+
+cd eQ/build
+
+#  cmake -D CMAKE_CXX_COMPILER=icc ..
+make
+
