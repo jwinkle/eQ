bece3255e9bf7f893ebe56fb246727fb1907eb70
James J. Winkle <wink@utexas.edu>
2020-09-12 14:44:29 -0500

    made old sabine fenics library mwe

--- sabine/jbuild.sh
+++ sabine/jbuild.sh
@@ -2,9 +2,9 @@ module purge
 
 # module load cmake fenics PETSc MPFR/4.0.2-GCCcore-6.4.0
 
-# module load FENICS/2019.1.0-foss-2019b-Python-3.7.4
+module load FENICS/2019.1.0-foss-2019b-Python-3.7.4
 
-module load fenics/2019.1.0-intel-2019-Python-3.8
+# module load fenics/2019.1.0-intel-2019-Python-3.8
 
 
 echo "module list: $(module list)"
